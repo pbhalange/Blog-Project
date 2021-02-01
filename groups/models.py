adc92abfa16eee76773ca8f3da1908841124d951
pbhalange <prdnshbhalange6@gmail.com>
2021-02-02 04:33:35 +0530

    final changes. Completed project

--- groups/models.py
+++ groups/models.py
@@ -1,47 +1,50 @@
+from django.conf import settings
+from django.urls import reverse
 from django.db import models
 from django.utils.text import slugify
-from django.urls import reverse
+# from accounts.models import User
+
 import misaka
+
 from django.contrib.auth import get_user_model
+User = get_user_model()
 
-
-User = get_user_model()   # Default user model
-# Create your models here.
-
+# https://docs.djangoproject.com/en/1.11/howto/custom-template-tags/#inclusion-tags
+# This is for the in_group_members check template tag
 from django import template
 register = template.Library()
 
 
+
 class Group(models.Model):
-	name = models.CharField(max_length=250, unique=True)
-	slug = models.SlugField(allow_unicode=True, unique=True)
-	description = models.TextField(blank=True, default='')
-	description_html = models.TextField(blank=True, default='', editable=False)
-	members = models.ManyToManyField(User, through='GroupMember')
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User,through="GroupMember")
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def save(self, **kwargs, *args):
-		self.slug = slugify(self.name)
-		self.description_html = misaka.html(self.description)
-		super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
 
-	def get_absolute_url(self):
-		return reverse('groups:single', kwargs={'slug': self.slug})
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
 
-	class Meta:
-		ordering = ['name']
 
+    class Meta:
+        ordering = ["name"]
 
 
 class GroupMember(models.Model):
-	group = models.ForeignKey(Group, related_name='membership' on_delete=models.CASCADE)
-	user = models.ForeignKey(User, related_name='user-group' on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="memberships")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_groups')
 
-	def __str__(self):
-		return self.user.username
+    def __str__(self):
+        return self.user.username
 
-	class Meta:
-		unique_together = ('group','user')
-		
+    class Meta:
+        unique_together = ("group", "user")
